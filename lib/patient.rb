14dff1d2594ee1a9c31725a41139bac618c8678e
Eli Huebner <elimhuebner@gmail.com>
2020-09-22 20:04:04 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -12,6 +12,14 @@ class Patient
     Appointment.new(date, self, doctor)
   end
   
+  def appointments
+    Appointment.all.select {|appointment| appointment.patient == self}
+  end
+  
+  def doctors
+    self.appointments.map {|appointment| appointment.doctor}
+  end
+  
   def self.all
     @@all
   end
